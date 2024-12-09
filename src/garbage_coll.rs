93462966e1e2225d92b4776fc48e355b36d14497
Elliot-Fomin <efomin@umd.edu>
2024-12-09 18:12:13 -0500

    1

--- src/garbage_coll.rs
+++ src/garbage_coll.rs
@@ -16,7 +16,9 @@ where P: AsRef<Path>, {
 pub fn reference_counting(filename: &str) ->  RefCountMem {
     // given code iterates through the lines in the file; you may mpdify it if desired
  
-    todo!(); // Define any variables to store the data as you go
+    let mut s: Vec<Vec<u32>> = vec![]; 
+    let mut h: Vec<(Option<Vec<u32>>, u32)> = vec![(None, 0); 10];
+
 
     let heap_ref_re = Regex::new(r"Ref Heap (([0-9]+) ?)*").unwrap();
     let stack_ref_re = Regex::new(r"Ref Stack (([0-9]+) ?)*").unwrap();
@@ -32,7 +34,17 @@ pub fn reference_counting(filename: &str) ->  RefCountMem {
                 }); // i.e. Ref Heap 0 1 2 -> numbers is now: [0, 1, 2]. 
                 // numbers is given to you and will correctly represent the file, you just need to handle the garbage collection now
 
-                todo!() // handle case of Ref Heap here
+                let index = numbers.remove(0);
+                h[index as usize].0 = Some(numbers.clone());
+                for number in numbers{
+                    h[number as usize].1 = h[number as usize].1 + 1;
+                    if h[number as usize].0 == None{
+                        h[number as usize].0 = Some(vec![]);
+                    }
+                }
+
+
+
 
             } else if stack_ref_re.is_match(&line) {
                 let mut numbers: Vec<u32> = vec![]; // vec of the numbers in the line
@@ -42,11 +54,26 @@ pub fn reference_counting(filename: &str) ->  RefCountMem {
                 }); // i.e. Ref Stack 0 1 2 -> numbers is now: [0, 1, 2]
                 // numbers is given to you and will correctly represent the file, you just need to handle the garbage collection now
 
-                todo!() // handle case of Ref Stack here
+                s.push(numbers.clone());
+                for number in numbers{
+                    h[number as usize].1 += 1;
+                    if h[number as usize].0 == None{
+                        h[number as usize].0 = Some(vec![]);
+                    }
+                }
 
             } else if pop_re.is_match(&line) {
                 
-                todo!() // handle Pop case here
+                let mut numbers = s.remove(s.len()-1);
+                let mut i = 0;
+                while i < numbers.len(){
+                    let index = *numbers.get(i).unwrap();
+                    h[index as usize].1 -= 1;
+                    if h[index as usize].1 == 0{
+                        numbers.extend(h[index as usize].0.clone().unwrap())
+                    }
+                    i+=1;
+                }
 
             } else {
                 panic!("no matches");
@@ -54,9 +81,18 @@ pub fn reference_counting(filename: &str) ->  RefCountMem {
         }
     };
 
-    // If you haven't already, free all memory that has 0 references here
-    // then, return the RefCountMem struct
-    todo!()
+    for index in &mut h{
+        if index.1 == 0{
+            index.0 = None;
+        }
+    }
+
+    let output = RefCountMem {
+        stack: s,
+        heap: h,
+    };
+
+    output
 
 }
 
@@ -64,14 +100,83 @@ pub fn reference_counting(filename: &str) ->  RefCountMem {
 // Takes in some form of stack and heap and returns all indicies in heap
 // that can be reached.
 pub fn reachable(stack: &Vec<Vec<u32>>, heap: &Vec<Option<(String, Vec<u32>)>>) -> Vec<u32> {
-    todo!()
+    let mut canReach: Vec<u32> = vec![];
+    for lst in stack{
+        for index in lst{
+            if !canReach.contains(&index){
+                canReach.push(*index);
+            }
+        }   
+    }
+    let mut index = 0;
+    while index < canReach.len(){
+        let i = canReach.get(index).unwrap();
+        for j in heap[*i as usize].clone().unwrap().1{
+            if !canReach.contains(&j){
+                canReach.push(j);
+            }
+        }
+        index +=1;
+    }
+    canReach
 } 
 
 pub fn mark_and_sweep(mem: &mut Memory) -> () {
-    todo!()
+    let reachable: Vec<u32> = reachable(&mem.stack,&mem.heap);
+    let mut i =0;
+    while i < mem.heap.len(){
+        if !reachable.contains(&(i as u32)){
+            mem.heap[i] = None;
+        }
+        i+=1;
+    }
 }
 // alive says which half is CURRENTLY alive. You must copy to the other half
 // 0 for left side currently in use, 1 for right side currently in use
+
 pub fn stop_and_copy(mem: &mut Memory, alive: u32) -> () {
-    todo!()
+
+    let mut startAlive = alive * (mem.heap.len()/2) as u32;
+    let mut startDead = (1-alive)*(mem.heap.len()/2) as u32;
+    let reachable: Vec<u32> = reachable(&mem.stack,&mem.heap);
+    let mut newPos = HashMap::new();
+    while startAlive < mem.heap.len() as u32{
+        if reachable.contains(&startAlive){
+            mem.heap[startDead as usize] = mem.heap[startAlive as usize].clone();
+            newPos.insert(startAlive, startDead);
+            let keys: Vec<_> = newPos.keys().cloned().collect();
+    
+            println!("{:?}", keys); 
+
+            startDead +=1;
+        }
+        startAlive += 1;
+    }
+    while startDead < ((1-alive) * (mem.heap.len()/2) as u32) + (mem.heap.len()/2) as u32{
+        mem.heap[startDead as usize] = None;
+        startDead +=1;
+    }
+    let mut index = (1-alive)*(mem.heap.len()/2) as u32;
+    let fin = index + (mem.heap.len()/2) as u32;
+    while index < fin{
+        if let Some((name, vec)) = mem.heap[index as usize].clone(){
+            let mut newVec :Vec<u32> = vec![];
+            for i in vec{
+                newVec.push(*newPos.get(&i).unwrap());
+            }
+            mem.heap[index as usize] = Some((name, newVec));
+        }
+        index +=1;
+    }
+    let mut index = 0;
+    while index < mem.stack.len(){
+        let mut newVec :Vec<u32> = vec![];
+        for i in mem.stack[index as usize].clone(){
+            println!("{} stack",i);
+            newVec.push(*newPos.get(&i).unwrap());
+        }
+        mem.stack[index as usize] = newVec;
+        index +=1;
+    }
+
 }
